297569d5e2a4d3f865608f7e8ad0eec0a0c2f647
DaSheaJ <dashea.johnson.1@gmail.com>
2021-06-30 10:18:59 -0500

    initial commit

---
+++ LINQExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string>() { "Injustice 2", "Mortal Kombat", "Spider-Man" };
+
+            var sortedGames = videoGames.OrderBy(x => x.Length).ToList();
+
+            sortedGames.ForEach(Console.WriteLine);
+        }
+    }
+}
